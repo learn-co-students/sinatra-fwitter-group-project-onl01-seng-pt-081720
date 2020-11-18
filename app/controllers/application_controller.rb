0e87eb888511c0986ee814628ce1e9443d92c206
Cloudymolecule <baires82@gmail.com>
2020-11-17 21:33:35 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :"signup"
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save 
+      session[:id] = @user.id
+      redirect to "/index"
+    else
+      redirect to "/signup"
+    end
+  end
+    
+
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
-
+    
+    get "/tweets" do
+        @tweets = Tweet.all
+        erb :"index"
+    end
 
 end
