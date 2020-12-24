678dcfe4edb119f85750e0a019fd9120026b60ce
jazminmatos <jmatos1993@gmail.com>
2020-12-23 20:24:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions 
+    set :session_secret, "secure_password"
   end
 
+  helpers do 
+    
+    #use user_id from session to find the user in the database & return that user
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    #should return true if user_id is in the session
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,58 @@
 class UsersController < ApplicationController
+    #loads the homepage
+  get '/' do 
+    erb :index
+  end
 
+  #loads signup page
+  #sign up page cannot be viewed if logged in
+  get '/signup' do
+    if !logged_in?
+        erb :'/users/signup'
+    else
+        redirect '/tweets'
+    end
+  end
 
+  post '/signup' do
+    user = User.new(params)
+
+    #saves it to the database
+    #checks that they inputed password b/c save method wouldn't work otherwise
+    #checks that they inputed a username & email
+    if user.save && user.username.length > 0 && user.email.length > 0
+        #binding.pry
+        session[:user_id] = user.id
+        redirect '/tweets'
+    else
+        redirect '/signup'
+    end
+  end
+
+  #loads login page
+  get '/login' do
+    if !logged_in?
+        erb :'/users/login'
+    else
+        redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/tweets'
+    end
+  end
+
+  get '/logout' do
+        if logged_in?
+            session.clear
+            erb :'/users/logout'
+        else
+            redirect '/index'
+        end
+    end
 end
